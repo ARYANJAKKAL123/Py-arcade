49d6326a0c22a735c7a49def2de65a6d8be1c5ac
ARYANJAKKAL132 <aryanjakkal717@gmail.com>
2026-03-24 06:06:05 +0530

    Created the proper file structure and completed the basics part of first phase

---
+++ 01_fundamentals/basics.py
@@ -0,0 +1,47 @@
+#1 Variable Creation and Naming
+
+player_name = "Aryan"
+player_level = 100
+
+#2 Pythonic Naming Conventions
+
+This_is_Pythonic_Naming = ""
+
+#3 Expressions vs Statements
+
+Name = "Aryan Jakkal"
+print(Name)
+
+#4 Using Comments
+
+# This is the comment in the python 
+
+#5 Type Checking
+
+print(f"Checking the type of a variable is string eg Name: {isinstance(Name,str)}")
+
+#6 Type Casting
+
+number = int("255")
+
+number = number + 1
+print(number)
+
+#7 Multi-line Strings
+
+Multi_line_string = """
+This is the 
+multiline
+string
+"""
+print(Multi_line_string)
+
+#8 Basic Indentation
+
+def Indentation():
+    num1 = 10
+    num2 = 20
+    return num1 + num2
+
+indentation = Indentation()
+print(indentation)
